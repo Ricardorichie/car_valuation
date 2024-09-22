e7a92d26814ba11850147b1082a199b1677e3641
Richard Omoyele <109069693+Richiee-D@users.noreply.github.com>
2024-09-22 19:35:53 +0100

    Sqlite and some validations added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { create } from 'domain';
+import { CreateUserDto } from './dto/create-user.dto';
 
-@Controller('users')
+@Controller('auth') //changed from 'users' to 'auth' to create a new path for authentication
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    console.log('body', body);
+    return this.usersService.create(body);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  async create({ email, password, name }: CreateUserDto): Promise<User> {
+    const user = this.usersRepository.create({ email, password, name });
+    return this.usersRepository.save(user);
+  }
+}

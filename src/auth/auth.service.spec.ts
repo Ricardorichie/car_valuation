2d5092b508f84931698f2ebe7030f65416ee3674
Richard Omoyele <109069693+Richiee-D@users.noreply.github.com>
2024-10-15 19:51:38 +0100

    new test added

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -4,9 +4,11 @@ import { CreateUserDto } from '../../src/users/dto/create-user.dto';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
 import { UsersService } from '../users/users.service';
+import { before } from 'node:test';
 
 describe('AuthService', () => {
-  it('can be instantiated', async () => {
+  let authService: AuthService;
+  beforeEach(async () => {
     const fakeUsersService: Partial<UsersService> = {
       find: () => Promise.resolve([]),
       create: (body: CreateUserDto) =>
@@ -31,7 +33,21 @@ describe('AuthService', () => {
         },
       ],
     }).compile();
-    const authService = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+  });
+
+  it('can create the instance of the auth service', async () => {
     expect(authService).toBeDefined();
+  });
+  it('creates a new user with a salted and hashed password', async () => {
+    const user = await authService.signup({
+      email: 'someemail@gmail.com',
+      password: 'password',
+      name: 'name',
+    });
+    expect(user.password).not.toEqual('password');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
   });
 });

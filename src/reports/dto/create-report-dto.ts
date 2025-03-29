b8149dde4e2c71e389bee0ac79751153996a2aee
Richard Omoyele <109069693+Richiee-D@users.noreply.github.com>
2025-03-29 20:40:30 +0000

    one-to-many associatio beetween user and report has been implemented

---
+++ src/reports/dto/create-report-dto.ts
@@ -0,0 +1,42 @@
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsString()
+  @IsNotEmpty()
+  make: string;
+
+  @IsString()
+  @IsNotEmpty()
+  model: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(1930)
+  @Max(2050)
+  year: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0)
+  @Max(1000000)
+  mileage: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  price: number;
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReportDto } from './dto/create-report-dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './report.entity';
+import { Repository } from 'typeorm';
+import { User } from '../users/user.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+  create(createReportDto: CreateReportDto, user: User) {
+    const report = this.repo.create(createReportDto);
+    report.user = user;
+    return this.repo.save(report);
+  }
+
+  findAll() {
+    return `This action returns all reports`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} report`;
+  }
+
+  update(id: number) {
+    return `This action updates a #${id} report`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} report`;
+  }
+}
